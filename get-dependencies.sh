177f82e0f0c824a3661772ac61efcddf4de2c16d
juju4 <juju4@users.noreply.github.com>
2016-10-30 18:51:12 -0400

    no serverspec test - travis 'exited with 126', use my fork for selinux role dep

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -11,7 +11,8 @@ fi
 
 #[ ! -d $rolesdir/redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/redhat-epel
 [ ! -d $rolesdir/kbrebanov.epel ] && git clone https://github.com/kbrebanov/ansible-epel.git $rolesdir/kbrebanov.epel
-[ ! -d $rolesdir/kbrebanov.selinux ] && git clone https://github.com/kbrebanov/ansible-selinux.git $rolesdir/kbrebanov.selinux
+#[ ! -d $rolesdir/kbrebanov.selinux ] && git clone https://github.com/kbrebanov/ansible-selinux.git $rolesdir/kbrebanov.selinux
+[ ! -d $rolesdir/kbrebanov.selinux ] && git clone https://github.com/juju4/ansible-selinux.git $rolesdir/kbrebanov.selinux
 [ ! -d $rolesdir/kbrebanov.dkms ] && git clone https://github.com/kbrebanov/ansible-dkms.git $rolesdir/kbrebanov.dkms
 [ ! -d $rolesdir/kbrebanov.kernel_headers ] && git clone https://github.com/kbrebanov/ansible-kernel_headers.git $rolesdir/kbrebanov.kernel_headers
 #[ ! -d $rolesdir/kbrebanov.virtualbox ] && git clone https://github.com/kbrebanov/ansible-virtualbox.git $rolesdir/kbrebanov.virtualbox
