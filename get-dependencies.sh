462c4b1697dacd22b5d36abe18050712b66d406c
juju4 <juju4@users.noreply.github.com>
2016-10-30 20:07:20 -0400

    fix centos7 epel with updated role fork

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -9,8 +9,9 @@ else
 rolesdir=$(dirname $0)/..
 fi
 
-[ ! -d $rolesdir/redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
-[ ! -d $rolesdir/kbrebanov.epel ] && git clone https://github.com/kbrebanov/ansible-epel.git $rolesdir/kbrebanov.epel
+#[ ! -d $rolesdir/redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
+#[ ! -d $rolesdir/kbrebanov.epel ] && git clone https://github.com/kbrebanov/ansible-epel.git $rolesdir/kbrebanov.epel
+[ ! -d $rolesdir/kbrebanov.epel ] && git clone https://github.com/juju4/ansible-epel.git $rolesdir/kbrebanov.epel
 #[ ! -d $rolesdir/kbrebanov.selinux ] && git clone https://github.com/kbrebanov/ansible-selinux.git $rolesdir/kbrebanov.selinux
 [ ! -d $rolesdir/kbrebanov.selinux ] && git clone https://github.com/juju4/ansible-selinux.git $rolesdir/kbrebanov.selinux
 [ ! -d $rolesdir/kbrebanov.dkms ] && git clone https://github.com/kbrebanov/ansible-dkms.git $rolesdir/kbrebanov.dkms
