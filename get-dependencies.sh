ffbe1c2f9284ebba9e8c6c92b9303bc8b1e93e0f
juju4 <juju4@users.noreply.github.com>
2018-06-01 23:56:19 -0400

    get-dependencies: use my variant of kbrebanov.kernel_headers

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -15,7 +15,8 @@ fi
 #[ ! -d $rolesdir/kbrebanov.selinux ] && git clone https://github.com/kbrebanov/ansible-selinux.git $rolesdir/kbrebanov.selinux
 [ ! -d $rolesdir/kbrebanov.selinux ] && git clone https://github.com/juju4/ansible-selinux.git $rolesdir/kbrebanov.selinux
 [ ! -d $rolesdir/kbrebanov.dkms ] && git clone https://github.com/kbrebanov/ansible-dkms.git $rolesdir/kbrebanov.dkms
-[ ! -d $rolesdir/kbrebanov.kernel_headers ] && git clone https://github.com/kbrebanov/ansible-kernel_headers.git $rolesdir/kbrebanov.kernel_headers
+#[ ! -d $rolesdir/kbrebanov.kernel_headers ] && git clone https://github.com/kbrebanov/ansible-kernel_headers.git $rolesdir/kbrebanov.kernel_headers
+[ ! -d $rolesdir/kbrebanov.kernel_headers ] && git clone https://github.com/juju4/ansible-kernel_headers.git $rolesdir/juju4.kernel_headers
 #[ ! -d $rolesdir/kbrebanov.virtualbox ] && git clone https://github.com/kbrebanov/ansible-virtualbox.git $rolesdir/kbrebanov.virtualbox
 [ ! -d $rolesdir/kbrebanov.virtualbox ] && git clone https://github.com/juju4/ansible-virtualbox.git $rolesdir/kbrebanov.virtualbox
 [ ! -d $rolesdir/volatility ] && git clone https://github.com/juju4/ansible-volatility $rolesdir/juju4.volatility
